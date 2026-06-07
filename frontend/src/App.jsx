83e57abaccbee8533cacd5665bb6a248c04e361f
Sharanya Udayakumar <sharanyaudayakumar7@gmail.com>
2026-06-07 18:11:57 +0530

    Connected frontend to Render API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/analyze",
+        "https://codepilot-ai-5v1c.onrender.com/analyze",
         {
           repo_url: repoUrl,
         }
